3f754a68547d46795929e7028ef7259bec161748
鄭竣文 <kevin39961462@gmail.com>
2021-06-03 20:54:45 +0800

    model

---
+++ Web410703549/ViewModels/SignUpData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web410703549.ViewModels
+{
+    public class SignUpData
+    {
+        public string Name { get; set; }
+
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string NameMessage { get; set; }
+
+        public string AccountMessage { get; set; }
+
+        public string PasswordMessage { get; set; }
+
+        public string Message { get; set; }
+    }
+}
